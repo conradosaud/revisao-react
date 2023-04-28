db2e1fe876dae06115befc7cadca12e31ef4780f
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2023-04-28 16:42:58 -0300

    app de viagem completo
    Foi passado o javascript puro para o React com todas as funções e aprendemos como fazer o UseState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //let credito = 155;
+    const [ credito, alteraCredito ] = React.useState(155)
+
+    function adicionaCredito(){
+        alteraCredito( credito + 1000 );
+    }
+
+    function compraPassagem( valor ){
+		if( credito < valor){
+			alert("Você não possui créditos para comprar esta passagem")
+			return
+		}
+		alteraCredito( credito - valor )
+		alert("Passagem comprada com sucesso")
+	}
+
+    return (
+        <div>
+
+            <h1 >Conradito Viagens</h1>
+            <p>Conheço o mundo com preços acessíveis</p>
+            <button>Contato</button>
+            <button onClick={ ()=> adicionaCredito() } >Adicionar Crédito</button>
+
+            <p> Seu crédito é de <strong>R$ { credito } </strong> </p>
+
+            <h2> Viagens </h2>
+            <p> Nova York - R$ 1.200 <button onClick={()=>compraPassagem(1200)} > Comprar </button> </p>
+            <p> Londres - R$ 1.550 <button onClick={()=>compraPassagem(1550)} > Comprar </button> </p>
+
+        </div>
+    );
 }
 
 export default App;
+
